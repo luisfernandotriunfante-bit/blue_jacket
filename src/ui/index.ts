c06e61634ecf93d91b3e304152e55676aef9470e
luisfernandotriunfante-bit <luis.fernandotriunfante@gmail.com>
2026-08-15 14:40:34 -0400

    chore(ui): expose visual foundation primitives

---
+++ src/ui/index.ts
@@ -0,0 +1,8 @@
+export { ScrollTriunfanteBackdrop } from './animation/ScrollTriunfanteBackdrop'
+export { HoverSidebar } from './navigation/HoverSidebar'
+export type { SidebarItem } from './navigation/HoverSidebar'
+export { TopTabs } from './navigation/TopTabs'
+export type { TopTab } from './navigation/TopTabs'
+export { GlassSurface } from './primitives/GlassSurface'
+
+import './theme/foundation.css'
