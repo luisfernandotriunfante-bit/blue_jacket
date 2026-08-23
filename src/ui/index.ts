058f25c98469adb50445d48060959a88e37b6dc4
luisfernandotriunfante-bit <luis.fernandotriunfante@gmail.com>
2026-08-23 02:19:52 -0400

    Remove primitive visual legado do barrel

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,7 +2,16 @@ export { BlueJacketShell } from './BlueJacketShell'
 export { HoverSidebar } from './navigation/HoverSidebar'
 export type { SidebarItem } from './navigation/HoverSidebar'
 export { TopTabs } from './navigation/TopTabs'
-export type { TopTab } from './navigation/TopTabs'
-export { GlassSurface } from './primitives/GlassSurface'
+export {
+  PanelAlert,
+  PanelCard,
+  PanelEmptyState,
+  PanelInfoRow,
+  PanelKpi,
+  PanelPage,
+  PanelSectionHeader,
+  PanelStat,
+  PanelTabs,
+} from './pattern/PanelVisual'
 
 import './theme/foundation.css'
